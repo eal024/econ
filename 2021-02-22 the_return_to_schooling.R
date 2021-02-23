aa72c8925683a2e834d91c7112ae707752046be3
eal024 <e.lamoey@gmail.com>
2021-02-23 22:52:09 +0100

    IV example from Verbeek

--- 2021-02-22 the_return_to_schooling.R
+++ 2021-02-22 the_return_to_schooling.R
@@ -12,3 +12,15 @@ Schooling <- as_tibble(Schooling)
 
 # Table 1
 Schooling %>% lm( lwage76 ~ ed76 + exp76  + I(exp76^2) + black + smsa76 + south76, data = .) %>% summary()
+
+# Interpretation of schoolings effect on wage
+100*(exp(0.0074)-1)
+
+# If schooling is endogenous 
+Schooling %>% names()
+Schooling %>% lm( ed76  ~ age76 + I(age76^2) + black + smsa76 + south76 + nearc4 , data =. ) %>% summary()
+
+
+# Using IV;
+Schooling %>% estimatr::iv_robust( lwage76 ~  exp76  + I(exp76^2) + black + smsa76 + south76 + ed76 | nearc4 +  age76 + I(age76^2) + black + smsa76 + south76 , data = .) %>% summary()
+
